361d33b59cc4cf6cf2333bb50e2ab39f7f686f60
Daniel Maguire <dmaguire1996@gmail.com>
2025-06-15 21:54:03 +0100

    Added placeholders for bottom nav

---
+++ app/(tabs)/home.tsx
@@ -0,0 +1,28 @@
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.container}>
+            <Text style={styles.text}>Home screen</Text>
+          </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+});
---
+++ app/(tabs)/recipe-book.tsx
@@ -0,0 +1,21 @@
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function RecipeBook() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Recipe Book Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+});
